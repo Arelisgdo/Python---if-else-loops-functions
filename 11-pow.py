826b6cd20b5095390e114ad66532d1b57f0a35f9
arelisgdo <arelisgiraldomazo438@gmail.com>
2023-06-14 19:39:51 -0500

    python#2

---
+++ 11-pow.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+def pow(a, b):
+    if b >= 0:
+        result = 1
+        for _ in range(b):
+            result *= a
+    else:
+        result = 1.0 / pow(a, -b)
+    return result
+
